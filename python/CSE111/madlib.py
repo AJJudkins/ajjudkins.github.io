3ad8d733fddc6245bbe37fb74da4c850a51b6b34
Austin Judkins <snikdujad@yahoo.com>
2022-04-29 11:51:58 -0600

    commit

---
+++ python/CSE111/madlib.py
@@ -0,0 +1,18 @@
+adjectiveOne = input('adjective:')
+animalOne = input('animal:')
+verbOne = input('verb:')
+exclamationOne = input('exclamation:')
+verbTwo = input('verb:')
+verbThree = input('verb:')
+adjectiveTwo = input('adjective:')
+animalTwo = input('animal:')
+verbFour = input('verb in past tense:')
+nounOne = input('noun:')
+nounTwo = input('noun:')
+number = input('number:')
+
+
+output = f'The other day, I was really in trouble. It all started when I saw a very {adjectiveOne} {animalOne} {verbOne} down the hallway. {exclamationOne.capitalize()}! I yelled. But all I could think to do was to {verbTwo} over and over. Miraculously, that caused it to stop, but not before it tried to {verbThree} right in font of my family.'
+print(output)
+outputTwo = f'After the {adjectiveTwo} {animalTwo} {verbFour}, I was finally able to get my homework done for my {nounOne} class and my {nounTwo} let me watch the new Star Wars, Episode {number}.'
+print(outputTwo)
